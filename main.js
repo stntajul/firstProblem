09c922e5f71294dfbbc9d0d04534638eee9bddd4
tajul islam <tajul.stn99@gmail.com>
2021-10-29 10:40:01 +0600

    js file updated

---
+++ main.js
@@ -0,0 +1,15 @@
+fetch('https://todn22mvx9.execute-api.ap-south-1.amazonaws.com/dev/access',{
+    method: 'POST',
+    headers: {'content-type': 'application/json',
+        
+        'x-api-key':'JU9eKYSwUW6lVlvGKrUkF71P1aybSR9y73jZ00y0'},
+    body: JSON.stringify({
+        "email": "tajul.stn99@gmail.com"
+    })
+
+})
+
+const xhr=new XMLHttpRequest();
+xhr.open('GET','https://todn22mvx9.execute-api.ap-south-1.amazonaws.com/dev/access?email=tajul.stn99@gmail.com')
+xhr.send();
+  
